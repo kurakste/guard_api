77fc16b7225144df6b68ef95ccb5e34d76548e57
Stepan <kurakste@gmail.com>
2019-08-25 22:37:22 +0300

    add post user

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,21 +1,35 @@
 const apiResponseObject = require('../helpers/getApiResponseObject');
+const models = require('../../models');
+
+const { User } = models;
 
 const userController = {
   getUser: async (ctx) => {
     ctx.body = `User controller, User vs ID id: ${ctx.params.id}`;
   },
+  // TODO: Check will it work well or no.
   postUser: async (ctx) => {
     const { body } = ctx.request;
+    // TODO: What about validation? Use sequelize? Write new function for it?
     const { user } = body;
-    if (!user) {
+    if (user) {
+      User.create(user)
+        .then(usr => {
+          // TODO: replace all console.log with loger system.
+          console.log(usr);
+          const resp = apiResponseObject(true, 'user object must be in request\'s body', { message: 'hellow!' });
+          ctx.body = JSON.stringify(resp);
+        })
+        .catch(err => {
+          console.log(err);
+          const resp = apiResponseObject(false, err.mesage);
+          ctx.body = JSON.stringify(resp);
+        });
+    } else {
       ctx.body = JSON.stringify(
         apiResponseObject(false, "user object must be in request's body"),
       );
-      return;
     }
-
-    console.log(user);
-    ctx.body = `Post User: ${JSON.stringify(user, null, '\t')}`;
   },
 
   getAll: async (ctx) => {
