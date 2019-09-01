f0d0944ae6645229329ddb0800e4f652698cbed8
Stepan <kurakste@gmail.com>
2019-09-01 21:35:19 +0300

    add socket controlle stub

---
+++ src/socketControllers/index.js
@@ -0,0 +1,31 @@
+const socketController = {
+  newAlert: (data) => {
+    console.log('new alert: ', data);
+  },
+
+  trackUpdate: (data) => {
+    console.log('track update: ', data);
+  },
+
+  alertInWork: (data) => {
+    console.log('allert in work: ', data);
+  }, 
+
+  gbrSent: (data) => {
+    console.log('gbr sent: ', data);
+  },
+
+  alertDecline: (data) => {
+    console.log('alertDecline: ', data);
+  },
+
+  alertClose: (data) => {
+    console.log('alert Close : ', data);
+  }, 
+
+  disconnect: (data) => {
+    console.log('disconected: ', data);
+  },
+};
+
+module.exports = socketController;
