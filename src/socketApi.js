a5b877a0da2b138fc058d7431f3cdbe6dcab32a4
Stepan <kurakste@gmail.com>
2019-09-16 14:37:24 +0300

    some clean up

--- src/socketApi.js
+++ src/socketApi.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const IO = require('koa-socket-2');
 const cors = require('koa-cors');
 const cpEventEmitter = require('./cpSocketEventEmitter');
-const cpSocketController = require('./socketControllers/spSocketController');
+const cpSocketController = require('./socketControllers/сpSocketController');
 const appSocketController = require('./socketControllers/appSocketController');
 const authMiddleware = require('./helpers/authMiddleware');
 
@@ -21,6 +21,7 @@ appSock.use(cors());
 appIo.attach(appSock);
 cpIo.attach(appSock);
 
+
 appIo.on('connection', (socket) => {
   const appNewAlarm = appSocketController.appNewAlarm.bind(appSocketController, cpIo, socket);
   const appNewPointInTrack = appSocketController.appNewPointInTrack.bind(appSocketController, cpIo);
@@ -32,6 +33,9 @@ appIo.on('connection', (socket) => {
 
 
 const openCpIoSockets = [];
+// cpIo.use(async (ctx, next) => {
+//   console.log('+++++++++++++++++++++++++');
+// });
 cpIo.on('connection', (socket) => {
   socket.use(authMiddleware);
   const cpPickedUpAlarm = cpSocketController.cpPickedUpAlarm.bind(cpSocketController, cpIo);
@@ -48,13 +52,11 @@ cpIo.on('connection', (socket) => {
   socket.on('cpSignIn', cpSignIn);
   socket.on('cpPing', (data) => {
     //  console.log('ping: ', data);
-    const { token, user } = data;
+    const { token, user, payload } = data;
     const userParsed = JSON.parse(user);
-    console.log('ping: ', userParsed, token);
+    console.log('ping: ', data);
   });
-  
   try {
-    
     if (!uid) throw new Error('uid is required!');
     const usersIds = openCpIoSockets.map(el => el.uid);
     if (usersIds.indexOf(uid) !== -1) throw new Error('user with this ID connected');
