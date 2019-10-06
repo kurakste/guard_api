4ad000156d382c4a90d75250c4deafec5d211b2b
Stepan <kurakste@gmail.com>
2019-10-06 22:31:42 +0300

    comment cpUsers routes

--- src/router/index.js
+++ src/router/index.js
@@ -15,16 +15,16 @@ router.post('/restore-password-step-one', userController.postRestorePasswordStep
 router.post('/restore-password-step-two', userController.postRestorePasswordStepTwo);
 router.get('/user/:id', userController.getUser);
 router.post('/user-new-ap', userController.postNewAppUser);
-router.post('/user-new-cp', userController.postNewCPUser);
 router.get('/users/new-app-users', userController.getNewAppUsers);
-router.get('/users/new-cp-users', userController.getNewCpUsers);
-router.post('/users/decline-cp-user', userController.postDeclineCpUser);
-router.post('/users/decline-app-user', userController.postDeclineAppUser);
-router.post('/users/verify-app-user', userController.postVerifyAppUser);
-router.post('/users/verify-cp-user', userController.postVerifyCpUser);
-router.patch('/user', userController.patchUser);
-router.delete('/user/:id', userController.deleteUser);
-router.get('/users', userController.getAll);
+// router.post('/user-new-cp', userController.postNewCPUser);
+// router.get('/users/new-cp-users', userController.getNewCpUsers);
+// router.post('/users/decline-cp-user', userController.postDeclineCpUser);
+// router.post('/users/decline-app-user', userController.postDeclineAppUser);
+// router.post('/users/verify-app-user', userController.postVerifyAppUser);
+// router.post('/users/verify-cp-user', userController.postVerifyCpUser);
+// router.patch('/user', userController.patchUser);
+// router.delete('/user/:id', userController.deleteUser);
+// router.get('/users', userController.getAll);
 // -------------
 
 module.exports = router;
