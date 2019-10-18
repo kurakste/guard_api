ca14932e6332b597720210b713ca2b45f2f66f51
Stepan <kurakste@gmail.com>
2019-10-18 08:40:26 +0300

    add route pay-monthly-subscription

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ const router = new Router();
 const rootController = require('../controllers/root');
 const userController = require('../controllers/users');
 const billController = require('../controllers/bills');
+const paymentController = require('../controllers/payment');
 
 
 router.get('/', rootController.root);
@@ -16,6 +17,7 @@ router.post('/restore-password-step-two', userController.postRestorePasswordStep
 router.get('/user/:id', userController.getUser);
 router.post('/user-new-ap', userController.postNewAppUser);
 router.get('/users/new-app-users', userController.getNewAppUsers);
+router.post('/pay-monthly-subscription', paymentController.payMonthlySubscriptionInit);
 // router.post('/user-new-cp', userController.postNewCPUser);
 // router.get('/users/new-cp-users', userController.getNewCpUsers);
 // router.post('/users/decline-cp-user', userController.postDeclineCpUser);
