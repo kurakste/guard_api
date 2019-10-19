23af6f6b679b4cf9393a181013ae41ad483727b6
Stepan <kurakste@gmail.com>
2019-10-19 23:32:02 +0300

    clean up

--- src/router/index.js
+++ src/router/index.js
@@ -19,16 +19,6 @@ router.post('/user-new-ap', userController.postNewAppUser);
 router.get('/users/new-app-users', userController.getNewAppUsers);
 router.post('/pay-monthly-subscription', paymentController.payMonthlySubscriptionInit);
 router.get('/get-payment-page', paymentController.getPaymentPage);
-// router.post('/get-payment-from', paymentController.getPaymentForm);
-// router.post('/user-new-cp', userController.postNewCPUser);
-// router.get('/users/new-cp-users', userController.getNewCpUsers);
-// router.post('/users/decline-cp-user', userController.postDeclineCpUser);
-// router.post('/users/decline-app-user', userController.postDeclineAppUser);
-// router.post('/users/verify-app-user', userController.postVerifyAppUser);
-// router.post('/users/verify-cp-user', userController.postVerifyCpUser);
-// router.patch('/user', userController.patchUser);
-// router.delete('/user/:id', userController.deleteUser);
-// router.get('/users', userController.getAll);
 // -------------
 
 module.exports = router;
