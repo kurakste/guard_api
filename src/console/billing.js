187cef5b4b0359458050b5c177f0a5013d49ca33
Stepan <kurakste@gmail.com>
2019-10-26 22:11:24 +0300

    add billing method

--- src/console/billing.js
+++ src/console/billing.js
@@ -1,12 +1,45 @@
+const dotenv = require('dotenv');
 const models = require('../../models');
 
-const { User } = models;
+dotenv.config();
+
+const { User, Bill } = models;
+const fullBillingSum = process.env.BILLINGSUM;
+const dailyBillingSum = fullBillingSum / 30;
 
 async function doBilling() {
   const users = await User.findAll({
-    where: { role: 35, lowBallance: false },
+    where: { role: 35, active: true, lowBallance: false },
   });
-  console.log(users.length);
+
+  const a = users.map(el => addBillingRecordById(el.id));
+  await Promise.all(a);
+  const promBunch = users.map(el => updateBallanceById(el.id));
+  await Promise.all(promBunch);
+  console.log('===---- Done');
+  return null;
+}
+
+async function addBillingRecordById(id) {
+  const bill = new Bill();
+  bill.UserId = id;
+  bill.sum = -1 * dailyBillingSum;
+  bill.comment = 'Daily billing';
+  bill.operationType = 'Daily billing';
+  bill.isPaymentFinished = true;
+  await bill.save();
+  console.log('done addBillingRecordById for id: ', id);
+  return null;
+}
+
+async function updateBallanceById(id) {
+  const sum = await Bill.sum('sum', { where: { UserId: id } });
+  const user = await User.findByPk(id);
+  user.lowBallance = (sum < 0);
+  user.balance = sum;
+  await user.save();
+  console.log('done updateBallanceById for id: ', id);
+  return null;
 }
 
 module.exports = doBilling;
--- src/console/cron.js
+++ src/console/cron.js
@@ -1,3 +1,9 @@
 const doBilling = require('./billing');
 
-doBilling();
+async function main() {
+  await doBilling();
+  console.log('work done');
+  process.exit(0);
+}
+
+main();
