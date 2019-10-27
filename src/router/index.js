bdfba3aeb0490006e0ef4efac3887b3dc26122cb
Stepan <kurakste@gmail.com>
2019-10-27 21:49:04 +0300

    clean up

--- src/router/index.js
+++ src/router/index.js
@@ -3,15 +3,12 @@ const Router = require('koa-router');
 const router = new Router();
 const rootController = require('../controllers/root');
 const userController = require('../controllers/users');
-const billController = require('../controllers/bills');
 const paymentController = require('../controllers/payment');
 
 
 router.get('/', rootController.root);
 // ------------ user/users ---------------------------------
 router.post('/sign-in', userController.postSignIn);
-router.post('/replenishment', billController.replenishment);
-router.post('/billing', billController.billing);
 router.post('/restore-password-step-one', userController.postRestorePasswordStepOne);
 router.post('/restore-password-step-two', userController.postRestorePasswordStepTwo);
 router.get('/user/:id', userController.getUser);
