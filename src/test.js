76b058ca8f641b02c39b531fbe5f032df37f8584
Stepan <kurakste@gmail.com>
2019-10-28 20:45:03 +0300

    clean up

--- src/test.js
+++ src/test.js
@@ -3,8 +3,6 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const key = process.env.GGKEY;
-// const [lat, lon] = [69.355930, 88.187606];
-// const [lat, lon] = [48.720762, 44.508472];
 const [lat, lon] = [48.651774, 43.171773];
 
 decoder(lat, lon, key)
