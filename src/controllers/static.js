2c3e6fe281ed20a61f693a873172332ebe459103
Stepan <kurakste@gmail.com>
2019-11-03 21:53:52 +0300

    add agreement page

---
+++ src/controllers/static.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const Mustache = require('mustache');
+const fs = require('fs');
+const logger = require('../helpers/logger');
+
+const apiUrl = process.env.API_URL;
+
+const controller = {
+  getAgreementPage: async (ctx) => {
+    logger.info('getAgreementPage');
+    try {
+      const pt = `${__dirname}/../views/agreement.html`;
+      const template = fs.readFileSync(pt).toString('utf8');
+      Mustache.parse(template);
+      const body = Mustache.render(template, { apiUrl });
+      ctx.response.body = body;
+    } catch (err) {
+      logger.error(err.message);
+    }
+    return ctx;
+  },
+};
+
+module.exports = controller;
