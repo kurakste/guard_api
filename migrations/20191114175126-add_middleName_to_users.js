f617fa83786cca37fc18b585ded974def7334573
Stepan <kurakste@gmail.com>
2019-11-14 20:53:48 +0300

    add middle name migration

---
+++ migrations/20191114175126-add_middleName_to_users.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    up: (queryInterface, Sequelize) => {
+      return queryInterface.addColumn(
+        'Users',
+        'middleName',
+        {
+          type: Sequelize.STRING,
+        }
+      )
+    };
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn(
+      'Users',
+      'middleName'
+    );
+  }
+};
