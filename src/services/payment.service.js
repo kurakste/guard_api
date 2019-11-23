3f8770691392478a6ee672a8caf79f5bb99c276d
Stepan <kurakste@gmail.com>
2019-11-23 20:32:41 +0300

    some make up

--- src/services/payment.service.js
+++ src/services/payment.service.js
@@ -93,7 +93,7 @@ const paymentService = {
   unsubscribeAndRemoveData: async (uid) => {
     const res = await clearRebillId(uid);
     return res;
-  }
+  },
 };
 
 // ========================= helpers ===========================================
