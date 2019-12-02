44cf594e6aa5e436d108c0be9275b73824b624c5
Stepan <kurakste@gmail.com>
2019-12-02 14:47:25 +0300

    add serviceStatus logic

--- src/socketEventEmitters/appEventEmitters.js
+++ src/socketEventEmitters/appEventEmitters.js
@@ -1,8 +1,10 @@
-const Sequelize = require('sequelize');
-const models = require('../../models');
+// const Sequelize = require('sequelize');
+// const models = require('../../models');
+const userRoleWithMessage = require('./userRoleWithMessage');
+const logger = require('../helpers/logger');
 
-const { Op } = Sequelize;
-const { Alarm, Track } = models;
+// const { Op } = Sequelize;
+// const { Alarm, Track } = models;
 
 const appSocketEventEmitter = {
 
@@ -28,52 +30,62 @@ const appSocketEventEmitter = {
 
 
   srvSendAppState: async (socket, user) => {
-    const openTrack = await getOpenTrack(user);
-    const openAlarm = await getOpenAlarm(user);
-    const alarmHistory = await getAlarmHistory(user);
-
-    socket.emit('srvSendAppState', {
-      user, openTrack, openAlarm, alarmHistory,
-    });
+    const { role, id } = user;
+    const appUser = (role === 35 || role === 33 || role === 31);
+    let message = '';
+    if (appUser) {
+      if (user.role === 35) {
+        message = (user.isSubscribeActive)
+          ? 'Приложение готово к работе'
+          : userRoleWithMessage[user.role];
+      } else {
+        message = userRoleWithMessage[user.role];
+      }
+      socket.emit('srvSendAppState', {
+        user, serviceStatus: message,
+      });
+    } else {
+      logger.error(`srvSendAppState: user with id: ${id} is not valid app user`);
+    }
   },
 
 };
 
-async function getOpenTrack(user) {
-  const track = await Track.findOne({
-    where: {
-      UserId: user.id,
-      isActive: true,
-    },
-  });
-  const out = track ? track.dataValues : null;
-  return out;
-}
+// async function getOpenTrack(user) {
+//   const track = await Track.findOne({
+//     where: {
+//       UserId: user.id,
+//       isActive: true,
+//     },
+//   });
+//   const out = track ? track.dataValues : null;
+//   return out;
+// }
 
-async function getOpenAlarm(user) {
-  const alarm = await Alarm.findOne({
-    where: {
-      UserId: user.id,
-      status: {
-        [Op.lt]: 10,
-      },
-    },
-  });
-  const out = alarm ? alarm.dataValues : null;
-  return out;
-}
+// async function getOpenAlarm(user) {
+//   const alarm = await Alarm.findOne({
+//     where: {
+//       UserId: user.id,
+//       status: {
+//         [Op.lt]: 10,
+//       },
+//     },
+//   });
+//   const out = alarm ? alarm.dataValues : null;
+//   return out;
+// }
 
-async function getAlarmHistory(user) {
-  const alarms = await Alarm.findAll({
-    where: {
-      UserId: user.id,
-      status: {
-        [Op.gt]: 0,
-      },
-    },
-  });
-  const out = alarms ? alarms.map(el => el.dataValues) : null;
-  return out;
-}
+// async function getAlarmHistory(user) {
+//   const alarms = await Alarm.findAll({
+//     where: {
+//       UserId: user.id,
+//       status: {
+//         [Op.gt]: 0,
+//       },
+//     },
+//   });
+//   const out = alarms ? alarms.map(el => el.dataValues) : null;
+//   return out;
+// }
 
 module.exports = appSocketEventEmitter;
