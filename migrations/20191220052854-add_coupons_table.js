a99b44ddb11ef77dfa47fe7082070ab72761730f
Stepan <kurakste@gmail.com>
2019-12-20 08:45:43 +0300

    add migration for coupons

---
+++ migrations/20191220052854-add_coupons_table.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+   return queryInterface.createTable('Coupons', {
+
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+
+    couponId: {
+      allowNull: true,
+      type: Sequelize.STRING
+    },
+
+    UserId: {
+      allowNull: true,
+      type: Sequelize.INTEGER,
+    },
+
+    isDone: {
+      type: Sequelize.BOOLEAN,
+    },
+
+    subscriptionType: {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+    },
+
+    comment: {
+      allowNull: true,
+      type: Sequelize.STRING
+    },
+
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+
+  });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Coupons');
+  }
+};
